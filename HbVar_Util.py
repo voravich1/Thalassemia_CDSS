92832cbcfbe9c4e24ddf02fa0c5e10b55729a6b2
worawich <voravich1@hotmail.com>
2019-04-22 13:54:21 +0700

    - create testBed for test run - create ClassifyThalassemia Function in HbVarUtil   - complete phenotype mapper task

--- HbVar_Util.py
+++ HbVar_Util.py
@@ -1,8 +1,72 @@
 import sys
 
-def clasifyThalasemia(inputFile , knowledgeFile):
+def clasifyThalasemia(inputFile , knowledgeFile , outputFile):
+    phenotypeDB = {}
+    firstLine_Flag = True
+    result = []
 
-    input = open(inputFile)
+    # Read knowledge File and create knowledge Dictionary
+    with open(knowledgeFile) as knowledge:
+        for line in knowledge:
+            info = line.split()
+            HbVar = info[0]
+            HGVS = info[1]
+            Common_name = info[2]
+            Phenotype = info[3]
+
+            if firstLine_Flag == True:  #Skip header
+                info_header = info
+                firstLine_Flag=False
+                continue
+
+            value = (Phenotype,Common_name,HGVS)
+            phenotypeDB.update({HbVar: value})
+    knowledge.close()
+
+    # Read input File and map to knowledge dictionary (phenotype mapping)
+    with open(inputFile) as input:
+        for line in input:
+
+            if line[0]=="#":    # Skip header
+                continue
+
+            info = line.split()
+            svID = info[0]
+            chr = info[1]
+            pos = info[2]
+            consequence = info[3]
+            HbVarID = info[4]
+
+            if HbVarID in phenotypeDB:  #check key exist
+                hit = phenotypeDB.get(HbVarID)
+                result.append(hit)
+    input.close()
+
+
+    output = open(outputFile,'w')
+    firstColumn_Flag=True
+
+    output.write("Phenotype\tCommonName\tHGVS\n")
+    for data in result:
+        outputFormat = ""
+        for subData in data:
+
+            if firstColumn_Flag==True:
+                outputFormat = subData
+                firstColumn_Flag = False
+            else:
+                outputFormat = outputFormat + "\t" + subData
+
+        output.write(outputFormat + "\n")
+        firstColumn_Flag=True
+
+    output.close()
+
+
+
+
+
+
 
 
 
---
+++ testBed.py
@@ -0,0 +1,6 @@
+#!/Users/worawich/miniconda3/envs/Thalassemia_CDSS/bin/python
+
+import sys
+import HbVar_Util as hb
+
+hb.clasifyThalasemia(sys.argv[1],sys.argv[2],sys.argv[3])
